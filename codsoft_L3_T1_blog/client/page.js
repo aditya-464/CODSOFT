df0c45ace9aa6e07711aac924ce19b6446fc3d69
Aditya <adityagiri1911@gmail.com>
2023-08-25 09:40:44 +0530

    All blogs page created

--- codsoft_L3_T1_blog/client/page.js
+++ codsoft_L3_T1_blog/client/page.js
@@ -1,2 +1,8 @@
 const pagesSection = document.querySelector(".pages-section");
-pagesSection.style.marginTop = `${navHeight+1}`+"px";
+const homeInnerDiv = document.querySelector(".home-inner-div");
+const allBlogIntroduction = document.querySelector(".all-blogs-introduction");
+
+const homeInnerDivHt = homeInnerDiv.getBoundingClientRect().height;
+allBlogIntroduction.style.height = `${homeInnerDivHt-navHeight}`+"px";
+
+pagesSection.style.marginTop = `${navHeight}`+"px";
