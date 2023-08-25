4309fd8fb5dd1b7b6c46cda6003177fc0ecfd020
Aditya <adityagiri1911@gmail.com>
2023-08-25 14:25:35 +0530

    My blogs section created

--- codsoft_L3_T1_blog/client/page.js
+++ codsoft_L3_T1_blog/client/page.js
@@ -1,8 +1,12 @@
 const pagesSection = document.querySelector(".pages-section");
 const homeInnerDiv = document.querySelector(".home-inner-div");
 const allBlogIntroduction = document.querySelector(".all-blogs-introduction");
+const myBlogIntroduction = document.querySelector(".my-blogs-introduction");
+
 
 const homeInnerDivHt = homeInnerDiv.getBoundingClientRect().height;
 allBlogIntroduction.style.height = `${homeInnerDivHt-navHeight}`+"px";
+myBlogIntroduction.style.height = `${homeInnerDivHt-navHeight}`+"px";
+
 
 pagesSection.style.marginTop = `${navHeight}`+"px";
