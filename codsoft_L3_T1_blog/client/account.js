7f2986cc72bb0791824b63e4934c63d202566f95
Aditya <adityagiri1911@gmail.com>
2023-08-27 07:57:51 +0530

    profile image option in account section css fixed

--- codsoft_L3_T1_blog/client/account.js
+++ codsoft_L3_T1_blog/client/account.js
@@ -1,26 +1,43 @@
 const accountLeft = document.querySelector(".account-left");
 const accountRight = document.querySelector(".account-right");
-// const navbar = document.querySelector(".navbar-outer");
 const icon = document.querySelector(".options-list-icon");
+const optionsList = document.querySelector(".account-left .options-list");
+const logoutContainer = document.querySelector(".account-right .logout-container");
 
-// Setting margin-top for account section
-// let navHeight = navbar.getBoundingClientRect().height;
-// const accountSection = document.querySelector(".account-section");
+const footerHeight = footerSection.getBoundingClientRect().height;
 accountSection.style.marginTop = `${navHeight}`+"px";
-accountRight.style.paddingTop = `${navHeight}`+"px";
+accountRight.style.marginTop = `${navHeight}`+"px";
+accountRight.style.height = `${homeInnerDivHt-navHeight-footerHeight}`+"px";
+logoutContainer.style.paddingTop = `${0.2*homeInnerDivHt}`+"px";
 
 
 icon.addEventListener("click", () => {
     if(!icon.children[0].classList.contains("display-none")){
+        optionsList.style.marginTop = `${icon.getBoundingClientRect().height+10}`+"px";
         accountLeft.style.translate = "0 0";
         accountRight.style.opacity = "0.1";
         icon.children[0].classList.add("display-none");
         icon.children[1].classList.remove("display-none");
     }
     else{
+        optionsList.style.marginTop = `${icon.getBoundingClientRect().height+10}`+"px";
         accountLeft.style.translate = "-120% 0";
         accountRight.style.opacity = "1";
         icon.children[0].classList.remove("display-none");
         icon.children[1].classList.add("display-none");
     }
+})
+
+// profile image option
+const imgCancelBtn = document.querySelector(".profile-image-container-right .imageUpload .cancel");
+
+imgCancelBtn.addEventListener("click", ()=>{
+    const inputFile = document.querySelector(".profile-image-container-right .imageUpload input");
+    const showFileName = document.querySelector(".profile-image-container-right .imageUpload .image-name");
+    let emptyFile = document.createElement('input');
+    emptyFile.type = 'file';
+    emptyFile.classList.add("inp");
+    inputFile.files = emptyFile.files;
+    showFileName.textContent = "Select Image";
+    imgCancelBtn.style.visibility = "hidden";
 })
